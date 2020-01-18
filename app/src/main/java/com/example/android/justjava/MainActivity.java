889a88c51fc81ac5249556a887be6728839c4504
Ratnesh Navlakhe <ratnesh061288@gmail.com>
2020-01-19 00:50:38 +0530

    Add price label, text and increment, decrement button to update the quantity of coffee.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.android.justjava;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void submitOrder(View view) {
+        displayPrice(quantity * 5);
+    }
+
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+        if (quantity > 0) {
+            quantity = quantity - 1;
+            display(quantity);
+        }
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
 }
